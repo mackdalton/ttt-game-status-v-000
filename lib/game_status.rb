daeff971e91b4faa737706e6bec7ccf42908d1bd
Mack Dalton <mackdalton@gmail.com>
2020-08-05 18:10:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,8 +17,13 @@ WIN_COMBINATIONS = [
   
 def won?(board)
  WIN_COMBINATIONS.each do |combos|
-   board[combos]
- end
+   if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
+    return win_combo
+  elsif board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
+     return win_combo
+    end
+  end
+    return false
  end
   
 
